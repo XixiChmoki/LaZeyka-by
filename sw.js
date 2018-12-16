40ee2a9c838f0a2ed2fbc0f794c937f91dd882a6
XixiChmoki <30309180+XixiChmoki@users.noreply.github.com>
2018-12-16 20:48:35 +0300

    dfghdfgh
    dfgh

---
+++ sw.js
@@ -0,0 +1,14 @@
+'use strict';
+
+importScripts('sw-toolbox.js');
+
+toolbox.precache([
+	"index.html",
+	"css/styles.css"
+	]);
+
+toolbox.router.get('/img/*', toolbox.cacheFirst);
+
+toolbox.router.get('/*', toolbox.networkFirst, {
+  networkTimeoutSeconds: 5
+});
