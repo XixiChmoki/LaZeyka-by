070524cee0f292a29d47603b2fa59853d3589c2f
XixiChmoki <30309180+XixiChmoki@users.noreply.github.com>
2019-02-03 14:35:39 +0300

    vbnm,
    bn,mb

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,18 @@
 $(function(){
+	// Progressive Web Apps
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 	
 	/* Выпадающее меню Начало*/
 	$('.menuToggle').on('click', function(){
